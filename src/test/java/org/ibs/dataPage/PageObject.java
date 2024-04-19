7cc99f69b3bda8c7279358075cb5ef7a6c2be4a9
kaynova93 <kaynova.93@mail.ru>
2024-04-19 23:10:22 +0300

    fix

--- src/test/java/org/ibs/dataPage/PageObject.java
+++ src/test/java/org/ibs/dataPage/PageObject.java
@@ -1,5 +1,6 @@
 package org.ibs.dataPage;
 
+import org.ibs.properties.PropManedger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -9,21 +10,17 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.CacheLookup;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-
 import java.net.MalformedURLException;
 import java.net.URI;
-
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
-import static org.ibs.properties.PropManedger.propertyMap;
-
 public class PageObject {
 
     private static final String URL_LOCAL = "http://localhost:8080/food";
-    private static final String URL = "http://149.154.71.152:8080/";
+    private static final String URL = "http://149.154.71.152:8080/food";
     public static WebDriver driver;
 
 
@@ -110,20 +107,21 @@ public class PageObject {
     }
 
 
-
     public void init() {
         System.out.println(System.getProperty("app.type.driver"));
-        if (System.getProperty("app.type.driver").equalsIgnoreCase("remote")){
-            initRemoteDriver();
-        }else{
-        driver = new ChromeDriver();
-        System.setProperty("webdriver.chromedriver.driver", "src/test/resources/chromedriver.exe");
-        driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
-        driver.get(URL_LOCAL);}
+        if (!String.valueOf(System.getProperty("app.type.driver")).equals("null")) {
+            if (System.getProperty("app.type.driver").equalsIgnoreCase("remote")) {
+                initRemoteDriver();}
+            } else {
+                driver = new ChromeDriver();
+                System.setProperty("webdriver.chromedriver.driver", "src/test/resources/chromedriver.exe");
+                driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+                driver.manage().window().maximize();
+                driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+                driver.get(URL_LOCAL);
+            }
+            PageFactory.initElements(driver, this);
 
-        PageFactory.initElements(driver, this);
     }
 
 
@@ -131,28 +129,19 @@ public class PageObject {
         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
         desiredCapabilities.setBrowserName(System.getProperty("app.type.browser"));
         desiredCapabilities.setVersion(System.getProperty("app.type.version"));
-//        desiredCapabilities.setCapability("selenoid:options",
-//                Map.<String,Object>of("enableVNC", true
-//                ));
-
-//        desiredCapabilities.setCapability("enableVideo", false);
-
+        desiredCapabilities.setCapability("selenoid:options",
+                Map.<String,Object>of("enableVNC", true
+                ));
         try {
-            driver = new RemoteWebDriver(URI.create("http://149.154.71.152:4444/wd/hub").toURL(),desiredCapabilities);
-//            driver = new RemoteWebDriver(URI.create(app.propertyMap.get("selenoid.url")).toURL(),desiredCapabilities);
-//
+            driver = new RemoteWebDriver(URI.create(PropManedger.propertyMap.get("selenoid.url")).toURL(),desiredCapabilities);
             driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
             driver.manage().window().maximize();
             driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
-            driver.get("http://149.154.71.152:8080/food");
+            driver.get(URL);
         } catch (MalformedURLException e) {
             throw new RuntimeException(e);
         }
-
-
     }
-
-
 
     public void close() {
         try {
@@ -177,6 +166,5 @@ public class PageObject {
                 findElement(By.tagName("tbody")).
                 findElements(By.cssSelector(" tr:last-child td")).stream().collect(Collectors.toList());
     }
-
 
 }
