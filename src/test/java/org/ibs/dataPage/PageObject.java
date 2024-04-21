a4f87125e2352c80715a6b9350058d6777df9b55
kaynova93 <kaynova.93@mail.ru>
2024-04-21 03:08:54 +0300

    test

--- src/test/java/org/ibs/dataPage/PageObject.java
+++ src/test/java/org/ibs/dataPage/PageObject.java
@@ -120,12 +120,11 @@ public class PageObject {
     private void initRemoteDriver(){
 
         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
-//        desiredCapabilities.setCapability ("browserName", System.getProperty("type.browser"));
-//        desiredCapabilities.setCapability ("browserVersion", System.getProperty("type.version"));
+        desiredCapabilities.setCapability ("browserName", System.getProperty("type.browser"));
+        desiredCapabilities.setCapability ("browserVersion", System.getProperty("type.version"));
         desiredCapabilities.setCapability("selenoid:options", Map.<String, Object>of(
-                "browserVersion", System.getProperty("type.version"),
-                "browserName", System.getProperty("type.browser"),
                 "enableVNC", true,
+                "enableLog", true,
                 "enableVideo", false
         ));
 
